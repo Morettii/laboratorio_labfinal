592a3c8020a5e66c9ae25b0f34009e8a168e0c20
Morettii <paolo.moretti.jimenez@cftmail.cl>
2022-12-07 03:53:16 -0800

    control final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EgresosComponent } from './egresos/egresos.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { VerEgresosComponent } from './ver-egresos/ver-egresos.component';
+import { VerUsuariosComponent } from './ver-usuarios/ver-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'egresos',
+    component: EgresosComponent
+  },
+  {
+    path: 'usuarios',
+    component: UsuariosComponent
+  },
+  {
+    path: 'ver_egresos',
+    component: VerEgresosComponent
+  },
+  {
+    path: 'ver_usuarios',
+    component: VerUsuariosComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { EgresosComponent } from './egresos/egresos.component';
+import { VerUsuariosComponent } from './ver-usuarios/ver-usuarios.component';
+import { VerEgresosComponent } from './ver-egresos/ver-egresos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    EgresosComponent,
+    VerUsuariosComponent,
+    VerEgresosComponent
   ],
   imports: [
     BrowserModule,
